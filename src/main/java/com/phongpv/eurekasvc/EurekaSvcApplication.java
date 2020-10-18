be22b160c2b2710835b5254cd03c225559504c12
Phong Phan <phan0001@risk.regn.net>
2020-10-18 13:44:30 +0700

    init project

---
+++ src/main/java/com/phongpv/eurekasvc/EurekaSvcApplication.java
@@ -0,0 +1,13 @@
+package com.phongpv.eurekasvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaSvcApplication.class, args);
+	}
+
+}
